fedc0eb78b49618791e90597bf3f06f8f6f6f92e
Bhumin Patel <bhumin2001@gmail.com>
2023-06-02 14:08:54 +0530

    Till lec-109 is finished

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -39,25 +40,42 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Age must be a postive number')
             }
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
 
-userSchema.statics.findByCredentials = async (email,password) =>{
-    const user = await User.findOne({email});
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
 
-    if(!user){
-        throw new Error('Unable to login');
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+
+    return token
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new Error('Unable to login')
     }
 
-    const isMatch = await bcrypt.compare(password,user.password)
+    const isMatch = await bcrypt.compare(password, user.password)
 
-    if(!isMatch){
-        throw new Error('Unable to login');
+    if (!isMatch) {
+        throw new Error('Unable to login')
     }
 
     return user
 }
 
+// Hash the plain text password before saving
 userSchema.pre('save', async function (next) {
     const user = this
 
